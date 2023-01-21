c06cc10dae2d12e1a2c9db3b53cb8621a37c8e92
Rajat <rajatmiddhaw92@gmail.com>
2023-01-21 14:32:01 +0530

    backend added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const postRoute=require('./routes/post')
 const conn= require('./connection/connection')
 const cors = require('cors')
-
+const port=  process.env.PORT  || 4500 
 conn()
 
 const app=express()
@@ -10,6 +10,8 @@ const app=express()
 app.use(cors())
 
 app.use(postRoute)
+app.get('/',(req,res)=>{
+    res.send('OK')
+})
 
-
-app.listen(4500)
+app.listen(port)
